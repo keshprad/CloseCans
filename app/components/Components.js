c8e13ea95a2f1c9c403280212e46c9a49da71372
Daniel <danieltomelee64@gmail.com>
2021-03-27 02:41:56 -0700

    Remove remnant of an unused file.

--- app/components/Components.js
+++ app/components/Components.js
@@ -1,6 +1,5 @@
 export { default as BinMarker } from "./BinMarker";
 export { default as ButtonHeader } from "./ButtonHeader";
-export { default as CheckButton } from "./CheckButton";
 export { default as FooterButton } from "./FooterButton";
 export { default as Hyperlink } from "./Hyperlink";
 export { default as Map } from "./Map";

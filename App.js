a30207246feecb52542d7b827f9561ec94b4de20
keshprad <keshav.r.pradeep@gmail.com>
2021-03-26 23:54:23 -0700

    Fixed imports for icons

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Container } from 'native-base';
 import { NativeRouter, Switch, Route } from 'react-router-native';
 import { default as AppLoading } from 'expo-app-loading';
 import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 // My imports
 import { About, Info, Home, AddBin } from './app/views/Views';
@@ -16,6 +17,7 @@ export default class App extends Component {
     await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
     });
 
     this.setState({ isReady: true });
